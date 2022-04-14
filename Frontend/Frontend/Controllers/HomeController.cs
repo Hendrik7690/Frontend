9d3fae9d0c822c5947fe907a782357f6cff89550
Hendrik <hendrik7690@gmail.com>
2022-04-14 11:45:28 +0700

    Master Commit

---
+++ Frontend/Frontend/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        string Baseurl = "https://jsonplaceholder.typicode.com";
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public async Task<ActionResult> Pelanggan()
+        {
+            List<Pelanggan> PelangganInfo = new List<Pelanggan>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("albums");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var PelangganResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Employee list  
+                    PelangganInfo = JsonConvert.DeserializeObject<List<Pelanggan>>(PelangganResponse);
+
+                }
+                //returning the employee list to view  
+                return View(PelangganInfo);
+            }
+        }
+
+            public ActionResult TambahData()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Frontend/Frontend/Models/Pelanggan.cs
@@ -0,0 +1,8 @@
+﻿public class Pelanggan
+{
+    public int userId { get; set; }
+    public int id { get; set; }
+
+    public string title { get; set; }
+
+}
